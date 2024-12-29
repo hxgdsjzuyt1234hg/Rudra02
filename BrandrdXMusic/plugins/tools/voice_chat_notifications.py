ce63e50a82a952488b3cb6df9e718f1e72877621
arvind0251 <160855021+arvind0251@users.noreply.github.com>
2024-12-29 16:18:07 +0530

    Update voice_chat_notifications.py

--- BrandrdXMusic/plugins/tools/voice_chat_notifications.py
+++ BrandrdXMusic/plugins/tools/voice_chat_notifications.py
@@ -1,1 +1,35 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+app = Client("my_bot")
+
+@app.on_message(filters.command("start_bot") & filters.user(OWNER_ID))
+async def start_bot(client: Client, message: Message):
+    await message.reply("Bot is now running!")
+
+@app.on_message(filters.command("stop_bot") & filters.user(OWNER_ID))
+async def stop_bot(client: Client, message: Message):
+    await message.reply("Bot is stopping...")
+    await app.stop()
+
+@app.on_message(filters.video_chat_started)
+async def vc_started(client: Client, message: Message):
+    chat_members = await app.get_chat_members(message.chat.id)
+    members_info = []
+    for member in chat_members:
+        user = member.user
+        members_info.append(f"{user.first_name} (@{user.username}) [ID: {user.id}]")
+    members_text = "\n".join(members_info)
+    await message.reply(f"The following members have joined the voice chat:\n{members_text}")
+
+@app.on_message(filters.video_chat_ended)
+async def vc_ended(client: Client, message: Message):
+    chat_members = await app.get_chat_members(message.chat.id)
+    members_info = []
+    for member in chat_members:
+        user = member.user
+        members_info.append(f"{user.first_name} (@{user.username}) [ID: {user.id}]")
+    members_text = "\n".join(members_info)
+    await message.reply(f"The following members have left the voice chat:\n{members_text}")
+
+app.run()
